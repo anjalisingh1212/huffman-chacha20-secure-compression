f326327ce5293b23071b8d2e351baa09088d8a27
anjalisingh1212 <anjali@DESKTOP-DLRB71G.localdomain>
2025-07-06 22:48:07 +0530

    utils updated

--- utils.c
+++ utils.c
@@ -1,14 +1,31 @@
-//Utils file
-
+//Utils.c
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "utils.h"
 
+// Function to handle errors by printing the error message and exiting
 void errExit(const char *str){
 	perror(str);
 	exit(EXIT_FAILURE);
 }
 
+const char *compressed_exts[] = {
+    ".jpg", ".png", ".mp3", ".zip", ".rar", ".gz", ".mp4", ".avi", ".pdf", NULL
+};
+
+int is_compressed_format(const char *filename) {
+    const char **ext = compressed_exts;
+    while (*ext) {
+        if(strstr(filename, *ext)) {
+            return 1; // Found a compressed format
+        }
+        ext++;
+    }
+    return 0;
+}
+// Function to print data in hexadecimal format
+// This is useful for debugging and visualizing binary data
 void printHex(const uint8_t *data, size_t len) {
     for (size_t i = 0; i < len; ++i) {
         printf("%02X ", data[i]);
--- utils.h
+++ utils.h
@@ -1,22 +1,34 @@
 // utils.h
-
 #ifndef UTILS_H__
 #define UTILS_H__
 
-/*
- * @brief Functions for utility
- */
-
 #include <stdint.h>
-#include <stddef.h>
 
+#define BUFFER_SIZE 4096    // Size of the buffer used for reading files
+#define MAGIC_HEADER "ENC"  // Magic header for identifying compressed files
+#define MAGIC_HEADER_LEN 3  // Length of the magic header
+
+/*
+* @brief Function to handle errors by printing the error message and exiting
+ * 
+ * @param str Error message to print
+ */
 void errExit(const char *str);
 
+/*
+ * @brief Function to check if a file is in a compressed format
+ * 
+ * @param filename Name of the file to check
+ * @return 1 if the file is in a compressed format, 0 otherwise
+ */
+int is_compressed_format(const char *filename);
+
+/*
+ * @brief Function to print data in hexadecimal format
+ * 
+ * @param data Pointer to the data to print
+ * @param len Length of the data
+ */
 void printHex(const uint8_t *data, size_t len);
-
-#define BUFFER_SIZE 4096
-#define MAGIC_HEADER "ENC"
-#define MAGIC_HEADER_LEN 3
-
 
 #endif  //UTILS_H__
